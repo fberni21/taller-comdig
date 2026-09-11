1d7aa037741d8eb29a19577ea74c86c2722d7403
Franco Berni <fberni@fi.uba.ar>
2026-09-11 19:44:02 -0300

    Add Huffman code builder

--- src/main.py
+++ src/main.py
@@ -1,17 +1,29 @@
 import argparse
 import source_encoder as se
+import numpy as np
 
 
 def main():
-    try:
-        parser = argparse.ArgumentParser()
-        parser.add_argument('filename', help='Input file name')
-        args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', help='Input file name')
+    args = parser.parse_args()
 
-        print(se.compute_file_stats(args.filename))
-    except Exception as e:
-        print(f'Error: {e}')
-        return 1
+    with open(args.filename, 'r') as file:
+        text = file.read()
+
+    probs = se.compute_char_probs(text)
+    entropy = se.compute_entropy(probs)
+    print(f'File entropy:\t\t{entropy:.4f} bits/char')
+
+    code_map = se.huff_encode(probs)
+    code_lengths = np.asarray([len(code_map[key]) for key in range(256)])
+    code_len_mean = np.sum(probs * code_lengths)
+    code_len_var = np.sum(probs * (code_lengths - code_len_mean)**2)
+
+    print(f'Minimum length:\t\t{entropy:.4f} bits/symbol')
+    print(f'Code mean length:\t{code_len_mean:.4f} bits/symbol')
+    print(f'Code efficiency:\t{entropy/code_len_mean:.4f}')
+    print(f'Code variance:\t\t{code_len_var:.4f} (bits/symbol)^2')
 
     return 0
 
--- src/source_encoder.py
+++ src/source_encoder.py
@@ -1,11 +1,43 @@
-def compute_text_stats(text):
-    return text
+import numpy as np
+import heapq
 
 
-def compute_file_stats(filename):
-    with open(filename, 'r') as file:
-        text = file.read()
-    return compute_text_stats(text)
+def compute_char_probs(text):
+    char_counts = np.zeros(256)
+    unique, counts = np.unique(list(text), return_counts=True)
+    for char, count in zip(unique, counts):
+        char_counts[ord(char)] = count
+    return char_counts / len(text)
+
+
+def compute_entropy(probs):
+    return -np.sum(probs * np.log2(probs + 1e-10))
+
+
+def huff_build_tree(probs):
+    tree = [(probs[c], c, None, None) for c in range(256)]
+    heapq.heapify(tree)
+    while len(tree) != 1:
+        t1 = heapq.heappop(tree)
+        t2 = heapq.heappop(tree)
+        new = (t1[0] + t2[0], -1, t1, t2)
+        heapq.heappush(tree, new)
+    return tree
+
+
+def huff_generate_code(node, b, code_map):
+    if node is None:
+        return code_map
+    code_map = huff_generate_code(node[2], b + '0', code_map)
+    if node[1] != -1:
+        code_map[node[1]] = b[:]
+    return huff_generate_code(node[3], b + '1', code_map)
+
+
+def huff_encode(probs):
+    tree = huff_build_tree(probs)
+    code_map = huff_generate_code(tree[0], '', {})
+    return code_map
 
 
 class SourceEncoder:
