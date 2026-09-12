0391b2de373fbe34727682c149baa088416364ce
Franco Berni <fberni@fi.uba.ar>
2026-09-12 11:03:14 -0300

    Explain Huffman algorithm implementation

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import argparse
-import source_encoder as se
 import numpy as np
+import source_encoder as se
+import string
 
 
 def main():
@@ -13,17 +14,29 @@ def main():
 
     probs = se.compute_char_probs(text)
     entropy = se.compute_entropy(probs)
-    print(f'File entropy:\t\t{entropy:.4f} bits/char')
+    print(f'File entropy\t: {entropy:.4f} bits/char')
 
     code_map = se.huff_encode(probs)
     code_lengths = np.asarray([len(code_map[key]) for key in range(256)])
     code_len_mean = np.sum(probs * code_lengths)
     code_len_var = np.sum(probs * (code_lengths - code_len_mean)**2)
 
-    print(f'Minimum length:\t\t{entropy:.4f} bits/symbol')
-    print(f'Code mean length:\t{code_len_mean:.4f} bits/symbol')
-    print(f'Code efficiency:\t{entropy/code_len_mean:.4f}')
-    print(f'Code variance:\t\t{code_len_var:.4f} (bits/symbol)^2')
+    print(f'Minimum length\t: {entropy:.4f} bits/symbol')
+    print(f'Code mean length: {code_len_mean:.4f} bits/symbol')
+    print(f'Code efficiency\t: {entropy/code_len_mean:.4f}')
+    print(f'Code variance\t: {code_len_var:.4f} (bits/symbol)^2')
+
+    for k, v in sorted(code_map.items(),
+                       key=lambda it: probs[it[0]],
+                       reverse=True):
+        p = probs[k]
+        if p == 0:
+            continue
+        c = f'`{chr(k)}`'
+        if chr(k) not in string.ascii_letters + ' ' + string.digits \
+                + string.punctuation:
+            c = f'0x{k:02x}'
+        print(f'{c}\t{p:.8f}\t{v}')
 
     return 0
 
--- src/source_encoder.py
+++ src/source_encoder.py
@@ -15,28 +15,58 @@ def compute_entropy(probs):
 
 
 def huff_build_tree(probs):
+    # Construye un árbol cuya forma final tiene en las hojas a los
+    # diferentes caracteres. Los caracteres más probables quedan a
+    # alturas más chicas, y los menos probables a alturas grandes.
+    # Es una forma estructurada de realizar el algoritmo de Huffman.
+
+    # Inicialmente, tenemos los nodos sueltos y son todos hojas
+    # (tienen un caracter `c` asociado, y sus hijos son None).
     tree = [(probs[c], c, None, None) for c in range(256)]
+
+    # El min-heap se usa para poder extraer los nodos de menor
+    # probabilidad en forma eficiente
     heapq.heapify(tree)
+
     while len(tree) != 1:
+        # Tomamos los dos nodos de menor probabilidad, y los hacemos
+        # hijos de un nuevo nodo cuya probabilidad es la suma de las
+        # probabilidades de sus hijos. Este nuevo nodo no tiene un
+        # caracter asociado (denotado por `-1`).
         t1 = heapq.heappop(tree)
         t2 = heapq.heappop(tree)
         new = (t1[0] + t2[0], -1, t1, t2)
         heapq.heappush(tree, new)
-    return tree
+
+    # La raíz del árbol es el único elemento
+    return tree[0]
 
 
 def huff_generate_code(node, b, code_map):
+    # Recorre recursivamente el árbol de Huffman, para asignar los
+    # códigos correspondientes.
+    #
+    # Las variables `b` y `code_map` inicialmente están vacías.
+    # - `b` acumula el código de una rama.
+    # - `code_map` es un diccionario donde se agregan todos los códigos.
+
     if node is None:
         return code_map
+
+    # Recorremos el sub árbol izquierdo, extendiendo con un `0`.
     code_map = huff_generate_code(node[2], b + '0', code_map)
+
+    # Si este nodo es hoja, le asignamos el código acumulado en `b`.
     if node[1] != -1:
         code_map[node[1]] = b[:]
+
+    # Recorremos el sub árbol derecho, extendiendo con un `1`.
     return huff_generate_code(node[3], b + '1', code_map)
 
 
 def huff_encode(probs):
     tree = huff_build_tree(probs)
-    code_map = huff_generate_code(tree[0], '', {})
+    code_map = huff_generate_code(tree, '', {})
     return code_map
 
 
